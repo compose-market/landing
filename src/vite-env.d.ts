e968a1ff6abf6f352e47aa6fe88b09f4572d22f3
jabyl <jabyl.new@gmail.com>
2026-06-08 02:12:41 +0200

    Declaring all new CSS modules

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,5 +2,9 @@
 
 declare module "@compose-market/theme/css";
 declare module "@compose-market/theme/css/app";
+declare module "@compose-market/theme/css/dark";
+declare module "@compose-market/theme/css/light";
 declare module "@compose-market/theme/css/effects";
 declare module "@compose-market/theme/css/market";
+declare module "@compose-market/theme/css/tokens";
+declare module "@compose-market/theme/css/utilities";
